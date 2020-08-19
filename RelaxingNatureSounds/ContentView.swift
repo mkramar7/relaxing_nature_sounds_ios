00f66a84f66045a1fd22df92101a0a3c6d1f1aad
Marko Kramar <marko.kramar7@gmail.com>
2020-08-19 19:37:19 +0200

    Initial Commit

---
+++ RelaxingNatureSounds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RelaxingNatureSounds
+//
+//  Created by Marko Kramar on 19/08/2020.
+//  Copyright © 2020 Marko Kramar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
