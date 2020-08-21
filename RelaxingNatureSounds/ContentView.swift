a786c352a2f8ed33f2390a6f5cf25c5604a77125
Marko Kramar <marko.kramar7@gmail.com>
2020-08-21 18:47:49 +0200

    Make multiple small improvements

--- RelaxingNatureSounds/ContentView.swift
+++ RelaxingNatureSounds/ContentView.swift
@@ -17,55 +17,57 @@ struct ContentView: View {
         List {
             ForEach(audioStore.allSounds, id: \.self) { sound in
                 Button(action: {
-                    self.currentlyPlaying = sound.id
-                    AudioPlayer.shared.playSound(sound.audio)
+                    if (self.currentlyPlaying == sound.id) {
+                        self.currentlyPlaying = ""
+                        AudioPlayer.shared.stopPlayingSound()
+                    } else {
+                        self.currentlyPlaying = sound.id
+                        AudioPlayer.shared.playSound(sound.audio)
+                    }
                 }) {
                     ZStack(alignment: .leading) {
                         Image(sound.image)
                             .resizable()
                             .aspectRatio(contentMode: .fill)
-                            .opacity(self.currentlyPlaying == sound.id ? 1 : 0.7)
+                            .opacity(self.isCurrentlyPlaying(sound) ? 1 : 0.8)
+                            .cornerRadius(8)
                             .animation(.spring())
                         
                         Triangle()
                             .fill(Color.white)
-                            .frame(width: 100, height: 50)
+                            .frame(width: 70, height: 35)
                             .rotationEffect(.degrees(90.0))
-                            .opacity(self.currentlyPlaying == sound.id ? 0.7 : 0)
-                            .padding(.leading, -40.0)
+                            .opacity(self.isCurrentlyPlaying(sound) ? 0.7 : 0)
+                            .padding(.leading, -17)
+                            .offset(x: 0, y: 5)
                             .animation(.spring())
                         
                         Text("\(sound.name)")
-                            .font(Font.custom("Luminari", size: 35))
+                            .font(Font.custom("Noteworthy", size: 50))
                             .foregroundColor(.white)
-                            .opacity(0.7)
-                            .padding(.leading, self.currentlyPlaying == sound.id ? 40 : 10)
+                            .opacity(0.8)
+                            .padding(.leading, self.isCurrentlyPlaying(sound) ? 40 : 10)
                             .animation(.spring())
                     }
-                    .padding(.bottom, 5)
+                    .padding(3)
+                    .background(Color.black)
                     .listRowInsets(EdgeInsets())
                 }
                 .buttonStyle(PlainButtonStyle())
             }
+        }.onAppear {
+            UITableView.appearance().backgroundColor = UIColor.black
+            UITableView.appearance().separatorStyle = .none
         }
+    }
+    
+    func isCurrentlyPlaying(_ sound: Sound) -> Bool {
+        self.currentlyPlaying == sound.id
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(audioStore: AudioStore())
-    }
-}
-
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-
-        return path
     }
 }
