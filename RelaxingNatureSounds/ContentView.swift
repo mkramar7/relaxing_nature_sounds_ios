6ec2180bbfeb65d3d894721117b6e3af26d9a889
Marko Kramar <marko.kramar7@gmail.com>
2020-10-05 12:04:00 +0200

    Make some refactoring, set iOS 14 as target OS

--- RelaxingNatureSounds/ContentView.swift
+++ RelaxingNatureSounds/ContentView.swift
@@ -12,16 +12,19 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack(alignment: .bottom) {
-                List {
-                    ForEach(AudioStore.allSounds) { sound in
-                        Button(action: {
-                            self.playOrStopPlaying(sound)
-                        }) {
-                            SoundImage(sound: sound)
+                ScrollView {
+                    VStack {
+                        ForEach(AudioStore.allSounds) { sound in
+                            Button(action: {
+                                self.playOrStopPlaying(sound)
+                            }) {
+                                SoundImageView(sound: sound)
+                            }
+                            .buttonStyle(PlainButtonStyle())
                         }
-                        .buttonStyle(PlainButtonStyle())
                     }
                 }
+                .padding(.bottom, 75)
                 
                 GoogleAdBanner()
             }
@@ -40,9 +43,6 @@ struct ContentView: View {
                     .scaleEffect(1.4)
             })
             .navigationBarTitle("Sounds of Nature")
-            .onAppear {
-                UITableView.appearance().separatorStyle = .none
-            }
         }
     }
     
@@ -56,41 +56,8 @@ struct ContentView: View {
     }
 }
 
-struct SoundImage: View {
-    @ObservedObject var sound: Sound
-    
-    var body: some View {
-        ZStack(alignment: .leading) {
-            Image(sound.image)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .opacity(sound.currentlyPlaying ? 1 : 0.8)
-                .cornerRadius(8)
-                .animation(.spring())
-            
-            Triangle()
-                .fill(Color.white)
-                .frame(width: 70, height: 35)
-                .rotationEffect(.degrees(90.0))
-                .opacity(sound.currentlyPlaying ? 0.8 : 0)
-                .padding(.leading, -17)
-                .offset(x: 0, y: 5)
-                .animation(.spring())
-            
-            Text("\(sound.name)")
-                .font(Font.custom("Noteworthy", size: 50))
-                .foregroundColor(.white)
-                .opacity(sound.currentlyPlaying ? 1 : 0.8)
-                .padding(.leading, sound.currentlyPlaying ? 40 : 10)
-                .animation(.spring())
-        }
-        .padding(3)
-        .listRowInsets(EdgeInsets())
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.dark)
     }
 }
---
+++ RelaxingNatureSounds/SoundImageView.swift
@@ -0,0 +1,48 @@
+//
+//  SoundImageView.swift
+//  RelaxingNatureSounds
+//
+//  Created by Marko Kramar on 05/10/2020.
+//  Copyright © 2020 Marko Kramar. All rights reserved.
+//
+
+import SwiftUI
+
+struct SoundImageView: View {
+    @ObservedObject var sound: Sound
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Image(sound.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .opacity(sound.currentlyPlaying ? 1 : 0.8)
+                .cornerRadius(8)
+                .animation(.spring())
+            
+            Triangle()
+                .fill(Color.white)
+                .frame(width: 70, height: 35)
+                .rotationEffect(.degrees(90.0))
+                .opacity(sound.currentlyPlaying ? 0.8 : 0)
+                .padding(.leading, -17)
+                .offset(x: 0, y: 5)
+                .animation(.spring())
+            
+            Text("\(sound.name)")
+                .font(Font.custom("Noteworthy", size: 50))
+                .foregroundColor(.white)
+                .opacity(sound.currentlyPlaying ? 1 : 0.8)
+                .padding(.leading, sound.currentlyPlaying ? 40 : 10)
+                .animation(.spring())
+        }
+        .padding(3)
+        .listRowInsets(EdgeInsets())
+    }
+}
+
+struct SoundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SoundImageView(sound: AudioStore.allSounds.first!)
+    }
+}
